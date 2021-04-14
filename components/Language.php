abfad35d3c1ebfe5ab9a80133558d123d557aa40
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-14 11:09:52 -0300

    🚧 Include language strings to start our development

--- components/Language.php
+++ components/Language.php
@@ -1,6 +1,82 @@
 <?php
     return [
-        'section_1' => [
-            'key_1' => 'value 1',
+        'section_titles' => [
+            'targets' => 'Para quem desenhamos essa formação',
+            'modules' => 'Os módulos de conteúdo que oferecemos',
+            'bonus' => 'Também tem bônus exclusivo para os alunos',
+            'about' => 'Sobre o instrutor da formação DWSW',
+            'faq' => 'Perguntas mais frequentes sobre a formação',
+            'disclaimer' => 'Uma informação importante sobre o conteúdo'
+        ],
+        
+        'headline' => [
+            'title' => 'Formação Desenvolvimento Web - SMALL WINS',
+            'subtitle' => 'Torne-se um desenvolvedor web profissional',
+            'callout' => 'Desenvolvimento web na prática: Do ZERO ao mercado de trabalho ainda em 2021',
+            'button_cta' => 'Quero me inscrever na Formação DWSW',
+        ],
+
+        'target' => 'Para qualquer um que deseja entrar no mercado de desenvolvimento web e tornar-se um programador profissional ainda em 2021',
+
+        'modules' => [
+            [ 'title' => 'Introdução', 'description' => 'Uma introdução básica da formação, do conteúdo, das ferramentas e do funcionamento geral da web - as bases para um bom desenvolvedor' ],
+            [ 'title' => 'Aprendendo HTML', 'description' => 'Finalmente aprenda o que é o HTML - elementos, atributos, semântica e tags básicas de cabeçalho de uma página' ],
+            [ 'title' => 'Aprendendo CSS', 'description' => 'Conceitos, regras, hierarquia, seletores e pseudo-classes. Além do basico de alinhamento e diferentes formas de exibir a mesma informação.' ],
+            [ 'title' => 'Aprendendo Javascript', 'description' => 'A programação da web. O que é o JS, objetos, eventos, tipos e fluxos de dados, funções e escopos. O básico para tornar seu site mais interativo e inteligente.' ],
+            [ 'title' => 'Comunicação Browser/Servidor e Ajax', 'description' => 'Reveja e aprofunde conceitos de cliente/servidor, além de entender o que é Ajax e como aplicar nos seus projetos para não deixar seu cliente esperando um resultado.' ],
+            [ 'title' => 'Tudo sobre React', 'description' => 'Uma interface de usuário em Javascript com tudo para você criar uma aplicação rica. Aprenda o que é e como usar o React em seus projetos.' ],
+            [ 'title' => 'Conhecendo o Git', 'description' => 'Bons desenvolvedores sabem como gerenciar seu código e suas versões. Você vai aprender os conceitos básicos do Git e como aplicar no seu dia a dia para facilitar o crescimento da sua aplicação.' ],
+            [ 'title' => 'Javascript no Servidor com NodeJS', 'description' => 'Dê ao seu sistema as informações que ele precisa através de um servidor NodeJS. Desde a instalação até a mão no código. Aprenda a manipular APIs e templates em Javascript no servidor.' ],
+            [ 'title' => 'Banco de dados MySQL', 'description' => 'Persistência de dados é fundamental em vários projetos. Entenda o que é um banco de dados, conheça o MySQL e como integrar seus recursos na aplicação, salvando dados para consultar depois.' ],
+        ],
+
+        'bonus' => [
+            'Aula exclusiva com a turma <b>toda semana</b>, via Zoom',
+            'E-book com CEM ideias de projetos para criar o portefólio de programador',
+            'Acesso a nossa comunidade secreta de programadores para aprimorar o networking',
+            'Uma aula exclusiva para ajudar a conseguir vagas de programador no Exterior',
+            'Efetivação do melhor da turma para trabalhar com a gente',
+        ],
+
+        'about_instructor' => [
+            'title' => [
+                'Douglas Corrêa',
+                'Desenvolvedor Web Full Stack',
+            ],
+            'biography' => [
+                'Douglas tem mais de 20 anos de experiência no mercado de tecnologia, sendo 15 deles dedicados exclusivamente para a Programação.',
+                'Nos últimos anos tem trabalhado remotamente para o Exterior (mesmo antes da pandemia) e com foco apenas nisso.',
+                'Sua missão, agora, é ajudar o máximo de pessoas a entrar num mercado com demanda crescente e com previsão de só aumentar.'
+            ],
+        ],
+
+        'faq' => [
+            [ 'title' => 'O que é esse curso?', 'answer' => 'A gente prefere chamar de FORMAÇÃO. Vamos te dar toda a base para começar a ser desenvolvedor web do zero e entrar no mercado de trabalho.' ],
+            [ 'title' => 'O que é esse tal de SMALL WINS?', 'answer' => 'É um método que nós criamos. SMALL WINS é um método baseado em pequenos passos e conquistas para, aos poucos, você alcançar a maestria no desenvolvimento web.' ],
+            [ 'title' => 'Como eu posso me inscrever?', 'answer' => 'Tem botões por toda a página que vão te levar direto para a página de inscrição. Basta você clicar em qualquer um deles e se inscrever.' ],
+            [ 'title' => 'Quando terei meu acesso liberado?', 'answer' => 'Assim que a plataforma processar seu pagamento e estiver tudo OK você vai receber no seu e-mail um usuário e senha para acessar nossas aula.' ],
+            [ 'title' => 'Como é o acesso às aulas do curso?', 'answer' => 'É uma plataforma online. Você pode acessar do celular, tablet ou computador. Tudo que você precisa é de acesso à internet e um dispositivo para acessar a plataforma.' ],
+            [ 'title' => 'Por quanto tempo as aulas ficarão disponíveis?', 'answer' => 'Nossas aula ficam disponíveis para você por um ano a partir da confirmação de pagamento. Acreditamos que é o suficiente para aprender, colocar tudo em prática e entrar no mercado de trabalho com tudo.' ],
+            [ 'title' => 'Há alguém para tirar dúvidas durante o curso?', 'answer' => 'SIM. Teremos mentores e o próprio professor tirando dúvidas durante o curso. Além, é claro, das nossas comunidades secretas de apoio e networking.' ],
+            [ 'title' => 'O que é a garantia incondicional mencionada?', 'answer' => 'É uma garantia da plataforma que garante a devolução do seu dinheiro EM ATÉ SETE DIAS caso você não esteja satisfeito com o nosso produto.' ],
+            [ 'title' => 'Estou pensando em entrar na próxima turma, que tal?', 'answer' => 'A gente não recomenda. Essa turma tem vagas limitadas e, por enquanto, não sabemos quando vamos abrir outra. A gente sugere que você não perca tempo e faça já sua inscrição.' ]
+        ],
+
+        'disclaimer' => [
+            'title' => 'Este produto não garante a obtenção de resultados',
+            'description' => 'Todo resultado possível segundo a Formação DWSW depende exclusivamente da dedicação e tempo disponibilizado pelo aluno.'
+        ],
+
+        'social_networks' => [
+            'facebook' => 'https://facebook.com/douglascorrea.io',
+            'instagram' => 'https://instagram.com/douglascorrea.io',
+            'github' => 'https://github.com/douglascorrea',
+            'twitter' => 'https://twitter.com/douglas_correa',
+            'youtube' => 'https://youtube.com/c/douglascorrea',
+        ],
+        
+        'footer' => [
+            'DS Correa - Consultoria em Informática',
+            '09.074.890/0001-50',
         ]
     ];
