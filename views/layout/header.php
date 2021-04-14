d6cbe72ba22eaab8ce318036b9b850ca36e25374
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-14 12:58:49 -0300

    🐛 Change rel to Google Fonts on header

--- views/layout/header.php
+++ views/layout/header.php
@@ -13,9 +13,8 @@
     <title>%APP_TITLE%</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Material+Icons">
-    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400&family=Work+Sans:ital,wght@0,400;0,700;1,400&display=swap">
-
+    <link rel="stylesheet" crossorigin="anonymous" href="https://fonts.googleapis.com/css2?family=Material+Icons">
+    <link rel="stylesheet" crossorigin="anonymous" href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400&family=Work+Sans:ital,wght@0,400;0,700;1,400&display=swap">
 
     <link rel="stylesheet" href="assets/reset.css">
     <link rel="stylesheet" href="assets/app.css">
