77df0a30e5f1e573c69b6475f8c5fad630c42844
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-14 12:59:11 -0300

    🚧 Include basic homepage

--- views/website/homepage.php
+++ views/website/homepage.php
@@ -1,1 +1,46 @@
+<section class="section__container">
+    <article class="section__article_wrapper" id="section__headline">
+        <h1><?php echo $this->language['headline']['title']; ?></h1>
+        <p><?php echo $this->language['headline']['subtitle']; ?></p>
+        
+        
+        <p><?php echo $this->language['headline']['callout']; ?></p>
+        <button class="button_cta"><?php echo $this->language['headline']['button_cta']; ?></button>
+    </article>
+</section>
 
+<section class="section__container">
+    <article class="section__article_wrapper">
+        <h1 class="section__title"><?php echo $this->language['section_titles']['targets']; ?></h1>
+    </article>
+</section>
+
+<section class="section__container">
+    <article class="section__article_wrapper">
+        <h1 class="section__title"><?php echo $this->language['section_titles']['modules']; ?></h1>
+    </article>
+</section>
+
+<section class="section__container">
+    <article class="section__article_wrapper">
+        <h1 class="section__title"><?php echo $this->language['section_titles']['bonus']; ?></h1>
+    </article>
+</section>
+
+<section class="section__container">
+    <article class="section__article_wrapper">
+        <h1 class="section__title"><?php echo $this->language['section_titles']['about']; ?></h1>
+    </article>
+</section>
+
+<section class="section__container">
+    <article class="section__article_wrapper">
+        <h1 class="section__title"><?php echo $this->language['section_titles']['faq']; ?></h1>
+    </article>
+</section>
+
+<section class="section__container">
+    <article class="section__article_wrapper">
+        <h1 class="section__title"><?php echo $this->language['section_titles']['disclaimer']; ?></h1>
+    </article>
+</section>
